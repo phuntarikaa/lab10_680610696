d2ddb9f42032543f8e3d8b8d1576cbf263693c98
phuntarikaa <phuntarikaa@gmail.com>
2025-12-24 18:01:09 +0700

    lab103

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+    int round=0;
+    double sum=0;
+    double osum=0;
+    ifstream tomato("score.txt");
+    string cilly;
+    while (getline(tomato,cilly)){
+        sum+=stod(cilly);
+        osum+=pow(stod(cilly),2);
+        round++;
+    }
+    
+    
+    
+    cout << "Number of data = "<<round<<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<<sum/round<<"\n";
+    cout << "Standard deviation = "<<sqrt((osum/round)-pow((sum/round),2));
+}
